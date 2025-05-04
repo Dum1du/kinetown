12e582c011bf13d618197dd527cf7946cd731e1a
Naveen Oshada <naveenoshada15@gmail.com>
2025-05-04 19:42:33 +0530

    Finish the Top rated movie page
    Need final color changing and review

--- src/pages/Trending/Trending.jsx
+++ src/pages/Trending/Trending.jsx
@@ -18,33 +18,34 @@ function Trending() {
   }, []);
 
 return (
-    <div className="flex flex-col min-h-screen w-full bg-white">
+    <div className="flex flex-col min-h-screen w-full bg-gray-900">
       <NavigationBar currentPage="trending" />
 
       <div className="text-center mt-10">
-        <h1 className="text-4xl font-bold text-gray-800 mb-2">IMDb Top-Rated Movies</h1>
-        <p className="text-gray-500">Most loved movies of all time</p>
+        <h1 className="text-4xl font-bold text-yellow-400 mb-2 mt-6">IMDb Top-Rated Movies</h1>
+        <p className="text-gray-100">Most loved 20 movies of all time</p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8">
-        {movies.map((movie) => (
-          <div
-            key={movie.id}
-            className="bg-white rounded-2xl shadow-md overflow-hidden transform transition duration-300 hover:scale-105"
-          >
-            <img
-              src={IMAGE_BASE_URL + movie.poster_path}
-              alt={movie.title}
-              className="w-full h-72 object-cover"
-            />
-            <div className="p-4">
-              <h2 className="text-xl font-semibold text-gray-800">{movie.title}</h2>
-              <p className="text-gray-600">{movie.release_date?.split("-")[0]}</p>
-              <p className="text-yellow-500 font-bold">⭐ {movie.vote_average}</p>
-            </div>
-          </div>
-        ))}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-1">
+  {movies.map((movie) => (
+    <div
+      key={movie.id}
+      className="group bg-gray-200 rounded-2xl shadow-md overflow-hidden transform transition duration-300 scale-80 hover:scale-85 hover:bg-white"
+    >
+      <img
+        src={IMAGE_BASE_URL + movie.poster_path}
+        alt={movie.title}
+        className="w-80 h-80 object-cover transition duration-300 group-hover:brightness-50"
+      />
+      <div className="p-4">
+        <h2 className="text-lg font-semibold text-gray-800">{movie.title}</h2>
+        <p className="text-gray-600">{movie.release_date?.split("-")[0]}</p>
+        <p className="text-yellow-500 font-bold">⭐ {movie.vote_average}</p>
       </div>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 }
